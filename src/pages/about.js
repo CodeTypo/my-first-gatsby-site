71c1702f6500d960bb9d302309338afe30e08d74
Dawid Kupczyk <dawid.kupczyk.dev@gmail.com>
2024-04-19 22:08:40 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About me"><main>
+    <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+  </main></Layout>
+  )
+}
+
+export const Head = () => (
+    <>
+      <title>About Me</title>
+      <meta name="description" content="Your description" />
+    </>
+  )
+export default AboutPage
